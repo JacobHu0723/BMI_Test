9f403f37a85199396717a56183ca959d8d51d779
JacobHu0723 <63916353+JacobHu0723@users.noreply.github.com>
2020-09-06 01:49:41 +0000

    Update BMI_Test.py

--- BMI_Test.py
+++ BMI_Test.py
@@ -1,6 +1,9 @@
 import time
 r=1
-print("欢迎使用体重指数(BMI)测试！\n开发者：Jacob_Hu\n本程序Github库地址:https://github.com/JacobHu0723/BMI_Test\n如有Bug请在Github中进行反馈！")
+print("欢迎使用体重指数(BMI)测试！\n开发者：Jacob_Hu\n本程序Github库地址:https://github.com/JacobHu0723/BMI_Test\n如有Bug请在Github下Issues进行反馈！")
+print("")
+time.sleep(1)
+print("体重指数(BMI)计算公式：BMI=体重(kg)/身高(m)^2\n\n按照《信息技术 必修一》P63实践活动标准\n性别\t等级\t体重指数/(kg·m^-2)\n男生\t正常\t16.5~23.2\n男生\t低体重\t≤16.4\n男生\t超重\t23.3~26.3\n男生\t肥胖\t≥26.4\n女生\t正常\t16.5~22.7\n女生\t低体重\t≤16.4\n女生\t超重\t22.8~25.2\n女生\t肥胖\t≥25.3\n")
 time.sleep(1)
 while r==1:
     s=int(input("请输入你的性别（男性请输1，女性请输2）:"))
@@ -13,22 +16,22 @@ while r==1:
     print("你的体重指数(BMI)为：",round(bmi,1))
     if s==1:
       if bmi>=16.5 and bmi<=23.2:
-        print("你的体重为正常")
+        print("你的体重","\033[4;32m正常\033[0m")
       elif bmi<=16.4:
-        print("你的体重过轻")
+        print("你的体重","\033[4;34m过轻\033[0m")
       elif bmi>=23.3 and bmi<=26.3:
-        print("你的体重超重")
+        print("你的体重","\033[4;33m超重\033[0m")
       elif bmi>=26.4:
-        print("你的体重肥胖")
+        print("你的体重""\033[4;31m肥胖\033[0m")
     elif s==2:
       if bmi>=16.5 and bmi<=22.7:
-        print("你的体重正常")
+        print("你的体重","\033[4;32m正常\033[0m")
       elif bmi<=16.4:
-        print("你的体重过轻")
+        print("你的体重","\033[4;34m过轻\033[0m")
       elif bmi>=22.8 and bmi<=25.2:
-        print("你的体重超重")
+        print("你的体重","\033[4;33m超重\033[0m")
       elif bmi>=25.3:
-        print("你的体重肥胖")
+        print("你的体重","\033[4;31m肥胖\033[0m")
     elif s!=1 and s!=2:
       print("性别输入错误")
     time.sleep(0.5)
